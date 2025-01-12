7815412ba495c92f2fd05505d1b9ca38875d81e9
Ritika Garg <ritikagarg60814@gmail.com>
2025-01-12 14:37:32 +0530

    removed dockerfile

--- frontend-app/frontendchat-app/src/app/app.module.ts
+++ frontend-app/frontendchat-app/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ChatComponent } from './components/chat/chat.component';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import {FormsModule} from '@angular/forms';
 
-const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:9000', options: {} };
 
 @NgModule({
   declarations: [
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const { Server } = require('socket.io')
-const server = new Server({cors :{ origin: 'http://localhost:4200'}});
+const { Server } = require('socket.io');
+const server = new Server({
+  cors: {
+    origin: 'http://localhost:4200'
+  }
+});
 
 server.on('connection', (socket) => {
-    console.log('connected');
-    socket.on('message', (data) => {
-        console.log(data);
-        socket.broadcast.emit('received', {data: data, 'message' : 'Message sent to server.'});  //this code sending data from server to client
-    })
+  console.log('connected');
+  socket.on('message', (data) => {
+    console.log(data);
+    socket.broadcast.emit('received', { data: data, message: 'Message sent to server.' });
+  });
+});
 
-    
-})
-
-server.listen(4000);
+console.log("Attempting to start the server...");
+server.listen(9000, (err) => {
+    if (err) {
+      console.error("Error starting server:", err);
+    } else {
+      console.log("Server running at port 9000");
+    }
+  });
